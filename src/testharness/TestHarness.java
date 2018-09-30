905f5c9bf27da4e544222e9b8e8d3dcec2b54e47
alex6p <alex5p@outlook.com>
2018-09-30 19:32:48 -0400

    Update TestHarness.java
    delete testcreateaccount controller and testupdate record controller

--- src/testharness/TestHarness.java
+++ src/testharness/TestHarness.java
@@ -47,54 +47,7 @@ public class TestHarness {
         
     }
     
-     /**
-     * Tests out the CreateAccountController in the alphacare package
-     * Outputs if the controller properly created the new account or not
-     */
-    Public static void testCreateAccountController(){
-        CreateAccountController cntl = new CreateAccountController();
 
-        //Data for new account
-        String testUserName = “Alex5p”;
-        String testPassWord =”422”;
-        
-        CreateAccount createAccount = acctCntl.createAccount(testUserName,testPassWord);
-        
-        //check if account was succesfully created
-        if(createAccount != null){
-            System.out.println(“Create Account Controller successfully created a new account”);
-        }
-        else {
-            System.out.println(“Create Account Controller failed to created a new account”);
-        }
-    }
-    
-     /**
-     * Tests out the updateRecordController in the alphacare package
-     * Outputs if the controller properly updated a record or not
-     */
-    Public static void testupdateRecordController(){
-        updateRecordController cntl= new updateRecordController();
-
-        //data for an updated record
-        String testSickness = “hot”;
-
-        ArrayList<String> testSymptoms = new ArrayList();
-        testSymptoms.add(“fever”);
-        testSymptoms.add(“congestion”);
-
-        String testMiscInfo = “Having trouble sleeping”;
-
-        Record testRecord = cntl.updateRecord(testSickness,testSymptoms,testMiscInfo);
-
-        //Check if the record was updated successfully
-        if(testRecord != null)
-	        System.out.println(“Update Record Controller successfully updated the record.”);
-        }
-        else{
-        System.out.println(“Create Update Record Controller failed to update the record.”);
-        }
-}
 
 
     
