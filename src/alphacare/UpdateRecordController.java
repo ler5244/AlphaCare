9c3a2561e4ad39349f0d715d781c5615b0545505
alex6p <alex5p@outlook.com>
2018-09-30 20:10:41 -0400

    Update UpdateRecordController.java
    Added Update Record Controller Test

--- src/alphacare/UpdateRecordController.java
+++ src/alphacare/UpdateRecordController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package alphacare;
 
 import java.util.ArrayList;
@@ -12,22 +7,25 @@ import java.util.ArrayList;
  * @author Laura
  */
 public class UpdateRecordController {
-   
+
     /**
- 	* Default constructor for the CreateRecordController class
- 	*/
+     * Default constructor for the CreateRecordController class
+     */
     public UpdateRecordController() {
-   	 
+
     }
-    
+
     /**
-    * Update a record for the user
-    * @param sickness sets the sickness for the user
-    * @param symptoms sets the symptoms for the user
-    * @param miscInfo sets the miscellaneous information for the user
-    */
-    public void updateRecord(String sickness, ArrayList<String> symptoms, String miscInfo){
-        Record record = new Record(sickness, symptoms, miscInfo);
+     * Update a record for the user
+     *
+     * @param sickness sets the sickness for the user
+     * @param symptoms sets the symptoms for the user
+     * @param miscInfo sets the miscellaneous information for the user
+     * @return 
+     */
+    public Record updateRecord(String sickness, ArrayList<String> symptoms, String miscInfo) {
+        return new Record(sickness, symptoms, miscInfo);
     }
-    
+
 }
+
