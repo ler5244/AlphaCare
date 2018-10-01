0937d1f864b092423220b0d0c27fd943c9a00f6b
alex6p <alex5p@outlook.com>
2018-09-30 20:08:30 -0400

    Update TestHarness.java
    Added  TestViewRecordController, TestCreateAccountController and TestUpdateRecordController

--- src/testharness/TestHarness.java
+++ src/testharness/TestHarness.java
@@ -1,8 +1,12 @@
 
 package testharness;
 
+import alphacare.CreateAccount;
+import alphacare.CreateAccountController;
 import alphacare.CreateRecordController;
 import alphacare.Record;
+import alphacare.UpdateRecordController;
+import alphacare.ViewRecordController;
 import java.util.ArrayList;
 
 /**
@@ -47,6 +51,80 @@ public class TestHarness {
         
     }
     
+    /**
+     * Tests out the CreateAccountController in the alphacare package Outputs if
+     * the controller properly created the new account or not
+     */
+    public static void testCreateAccountController() {
+        CreateAccountController cntl = new CreateAccountController();
+
+        //Data for new account
+        String testUserName = "Alex5p";
+        String testPassWord = "422";
+
+        CreateAccount createAccount = cntl.createAccount(testUserName, testPassWord);
+
+        //check if account was succesfully created
+        if (createAccount != null) {
+            System.out.println("Create Account Controller successfully created a new account");
+        } else {
+            System.out.println("Create Account Controller failed to created a new account");
+        }
+    }
+
+    /**
+     * Tests out the updateRecordController in the alphacare package Outputs if
+     * the controller properly updated a record or not
+     */
+    public static void testupdateRecordController() {
+        UpdateRecordController cntl = new UpdateRecordController();
+
+        //data for an updated record
+        String testSickness = "hot";
+
+        ArrayList<String> testSymptoms = new ArrayList();
+        testSymptoms.add("fever");
+        testSymptoms.add("congestion");
+
+        String testMiscInfo = "Having trouble sleeping";
+
+        Record testRecord = cntl.updateRecord(testSickness, testSymptoms, testMiscInfo);
+
+        // check if the record was successfully created
+        if (testRecord != null) {
+            System.out.println("Create Record Controller successfully created new record");
+        } else {
+            System.out.println("Create Record Controller failed to created new record");
+        }
+    }
+
+    /**
+     * Tests out the ViewRecordController in the alphacare package Outputs if
+     * the controller properly viewed the record or not
+     */
+    public static void testViewRecordController() {
+
+        ViewRecordController cntl = new ViewRecordController();
+
+        // data for the new record
+        String testSickness = "cold";
+
+        ArrayList<String> testSymptoms = new ArrayList();
+        testSymptoms.add("fever");
+        testSymptoms.add("congestion");
+
+        String testMiscInfo = "Having trouble sleeping";
+
+        Record testRecord = cntl.getRecord(testSickness, testSymptoms, testMiscInfo);
+
+        // check if the record is viewed successfully
+        if (testRecord != null) {
+            System.out.println("View Record Controller successfully viewed a record");
+        } else {
+            System.out.println("View Record Controller failed to view a record");
+        }
+
+    }
 
 
 
