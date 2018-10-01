097448d4852f40248f62bf02d041e14be1feddb3
alex6p <alex5p@outlook.com>
2018-09-30 20:11:21 -0400

    Update ViewRecordController.java
    Added View Record Controller Test

--- src/alphacare/ViewRecordController.java
+++ src/alphacare/ViewRecordController.java
@@ -1,33 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package alphacare;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Laura
  */
-
 public class ViewRecordController {
-     /**
-    * Default constructor for the viewRecordsController class
-    */
-    
+
+    /**
+     * Default constructor for the viewRecordsController class
+     */
     public ViewRecordController() {
-        
-        String sickness = "example";
-        String symptoms = "example";
-        String miscInfo = "example";
-        
+    }
+
+    /**
+     *
+     * @param sickness
+     * @param symptoms
+     * @param miscInfo
+     * @return
+     */
+    public Record getRecord(String sickness, ArrayList<String> symptoms, String miscInfo) {
+
+        return new Record(sickness, symptoms, miscInfo);
         //To be used when record is being is included.
         /*
         String sickness = Record.getSickness();
         String symptoms = Record.getSymptoms();
         String miscInfo = Record.getMiscInfo();
-        */
-         
+         */
+
     }
 
 }
