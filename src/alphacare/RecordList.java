c89e7459c44a33f7bec268bcecfe05bcae787a5f
Laura <Laura@104.38.112.0>
2018-11-16 21:24:31 -0500

    Persistent save record data

---
+++ src/alphacare/RecordList.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alphacare;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Laura
+ */
+public class RecordList {
+    private ArrayList<Record> record = new ArrayList(); 
+    
+    public RecordList(){
+        record = new ArrayList();
+        ArrayList<Record> newRecord = new ArrayList();
+        Record exampleRecord = new Record("sickness","symptoms", "miscInfo");
+                
+        newRecord.add(exampleRecord);
+        record = newRecord; 
+    }
+
+    public ArrayList<Record> getRecord() {
+        return record;
+    }
+
+    public void setRecord(ArrayList<Record> record) {
+        this.record = record;
+    }
+}
